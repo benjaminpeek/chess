e02c7b274fb8a10cb21c0b1aa23ca62ee5ed95ca
Benjamin Peek <benjaminpeek8@gmail.com>
2024-03-01 16:41:58 -0800

    added GameDAO interface and methods, added exceptions to User and Game

--- server/src/main/java/dataAccess/UserDataAccessObject.java
+++ server/src/main/java/dataAccess/UserDataAccessObject.java
@@ -3,6 +3,6 @@ package dataAccess;
 import model.UserData;
 
 public interface UserDataAccessObject {
-    UserData getUser(String username);
-    void createUser(String username, String password, String email);
+    UserData getUser(String username) throws DataAccessException;
+    void createUser(String username, String password, String email) throws DataAccessException;
 }
