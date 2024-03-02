5d6b67b6fa9351d79d42e3a266de82924a626f47
Benjamin Peek <benjaminpeek8@gmail.com>
2024-03-01 16:48:02 -0800

    added AuthDAO methods

--- server/src/main/java/dataAccess/AuthDataAccessObject.java
+++ server/src/main/java/dataAccess/AuthDataAccessObject.java
@@ -1,4 +1,9 @@
 package dataAccess;
 
+import model.AuthData;
+import model.UserData;
+
 public interface AuthDataAccessObject {
+    void createAuth(UserData username) throws DataAccessException;
+    void deleteAuth(AuthData authData) throws DataAccessException;
 }
