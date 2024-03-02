7b7c93d10b4f6291457bd4b5bf30404f052d4d13
Benjamin Peek <benjaminpeek8@gmail.com>
2024-03-01 16:27:16 -0800

    added UserDAO interface and methods

--- server/src/main/java/dataAccess/UserDataAccessObject.java
+++ server/src/main/java/dataAccess/UserDataAccessObject.java
@@ -1,4 +1,8 @@
 package dataAccess;
 
+import model.UserData;
+
 public interface UserDataAccessObject {
+    UserData getUser(String username);
+    void createUser(String username, String password, String email);
 }
