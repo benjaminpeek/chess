8665ef28f39ad9b26f00007dfc125ad1ad6def89
Benjamin Peek <benjaminpeek8@gmail.com>
2024-03-03 15:57:15 -0800

    created GameService endpoint Request classes

---
+++ server/src/main/java/service/request/CreateGameRequest.java
@@ -0,0 +1,29 @@
+package service.request;
+
+import java.util.Objects;
+
+public record CreateGameRequest(
+        String authToken,
+        String gameName
+) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CreateGameRequest that = (CreateGameRequest) o;
+        return Objects.equals(authToken, that.authToken) && Objects.equals(gameName, that.gameName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authToken, gameName);
+    }
+
+    @Override
+    public String toString() {
+        return "CreateGameRequest{" +
+                "authToken='" + authToken + '\'' +
+                ", gameName='" + gameName + '\'' +
+                '}';
+    }
+}
---
+++ server/src/main/java/service/request/JoinGameRequest.java
@@ -0,0 +1,31 @@
+package service.request;
+
+import java.util.Objects;
+
+public record JoinGameRequest(
+    String authToken,
+    String playerColor,
+    int gameID
+) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JoinGameRequest that = (JoinGameRequest) o;
+        return gameID == that.gameID && Objects.equals(authToken, that.authToken) && Objects.equals(playerColor, that.playerColor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authToken, playerColor, gameID);
+    }
+
+    @Override
+    public String toString() {
+        return "JoinGameRequest{" +
+                "authToken='" + authToken + '\'' +
+                ", playerColor='" + playerColor + '\'' +
+                ", gameID=" + gameID +
+                '}';
+    }
+}
