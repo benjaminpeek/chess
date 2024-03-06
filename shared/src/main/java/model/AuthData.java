fb521dbb6acbd0bb84f3c8ccc58566ad649cb7ee
Benjamin Peek <benjaminpeek8@gmail.com>
2024-03-06 14:36:32 -0700

    reformatted model classes and deleted unused methods

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -2,25 +2,7 @@ package model;
 
 import java.util.Objects;
 
-public record AuthData(String authToken, String username) {
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AuthData authData = (AuthData) o;
-        return Objects.equals(authToken, authData.authToken) && Objects.equals(username, authData.username);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(authToken, username);
-    }
-
-    @Override
-    public String toString() {
-        return "AuthData{" +
-                "authToken='" + authToken + '\'' +
-                ", username='" + username + '\'' +
-                '}';
-    }
-}
+public record AuthData (
+        String authToken,
+        String username
+) {}
--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -4,30 +4,10 @@ import chess.ChessGame;
 
 import java.util.Objects;
 
-public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GameData gameData = (GameData) o;
-        return gameID == gameData.gameID && Objects.equals(whiteUsername, gameData.whiteUsername)
-                && Objects.equals(blackUsername, gameData.blackUsername) && Objects.equals(gameName, gameData.gameName)
-                && Objects.equals(game, gameData.game);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(gameID, whiteUsername, blackUsername, gameName, game);
-    }
-
-    @Override
-    public String toString() {
-        return "GameData{" +
-                "gameID=" + gameID +
-                ", whiteUsername='" + whiteUsername + '\'' +
-                ", blackUsername='" + blackUsername + '\'' +
-                ", gameName='" + gameName + '\'' +
-                ", game=" + game +
-                '}';
-    }
-}
+public record GameData (
+        int gameID,
+        String whiteUsername,
+        String blackUsername,
+        String gameName,
+        ChessGame game
+) {}
