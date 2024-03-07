15653706746f12fa6065bbdee5729dc8338e078f
Benjamin Peek <benjaminpeek8@gmail.com>
2024-03-06 20:59:05 -0700

    finished, untested, implementing register endpoint

--- server/src/main/java/handlers/UserHandler.java
+++ server/src/main/java/handlers/UserHandler.java
@@ -1,13 +1,28 @@
 package handlers;
 
+import dataAccess.DataAccessException;
+import request.RegisterRequest;
 import response.RegisterResponse;
 import com.google.gson.Gson;
+import service.UserService;
 import spark.*;
 
 public class UserHandler {
-    public RegisterResponse registerHandler(Request req, Response res) {
+    private final UserService userService;
 
+    public UserHandler(UserService userService) {
+        this.userService = userService;
+    }
 
-        return new RegisterResponse("hi", "mo"); // temp
+    public String registerHandler(Request req, Response res) throws DataAccessException {
+        // take in the HTTP JSON request, turn it into a RegisterRequest object
+        RegisterRequest registerRequest = new Gson().fromJson(req.body(), RegisterRequest.class);
+        // use the appropriate service's method
+        RegisterResponse registerResponse = userService.registerService(registerRequest);
+        // convert the service response to json for the res body
+        res.body(new Gson().toJson(registerResponse));
+        res.status(200);
+
+        return res.body();
     }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,9 @@
 package server;
 
+import dataAccess.memory.MemoryAuthDataAccess;
+import dataAccess.memory.MemoryUserDataAccess;
+import handlers.UserHandler;
+import service.UserService;
 import spark.*;
 
 public class Server {
@@ -9,7 +13,16 @@ public class Server {
 
         Spark.staticFiles.location("web");
 
+        // prepare the necessary data access objects
+        MemoryUserDataAccess userDataAccess = new MemoryUserDataAccess();
+        MemoryAuthDataAccess authDataAccess = new MemoryAuthDataAccess();
+        // prepare the services
+        UserService userService = new UserService(userDataAccess, authDataAccess);
+        // prepare the handlers
+        UserHandler userHandler = new UserHandler(userService);
+
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", userHandler::registerHandler);
 
         Spark.awaitInitialization();
         return Spark.port();
