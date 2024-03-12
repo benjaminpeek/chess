e09d4dc62d4d68d83bcfabe286a3224232cd18f2
Benjamin Peek <benjaminpeek8@gmail.com>
2024-03-12 13:19:07 -0600

    implemented login endpoint, and tested

--- server/src/main/java/handlers/UserHandler.java
+++ server/src/main/java/handlers/UserHandler.java
@@ -1,8 +1,6 @@
 package handlers;
 
 import dataAccess.DataAccessException;
-import dataAccess.interfaces.AuthDataAccess;
-import dataAccess.interfaces.UserDataAccess;
 import exceptions.AlreadyTakenException;
 import exceptions.BadRequestException;
 import exceptions.UnauthorizedException;
@@ -17,52 +15,29 @@ import spark.*;
 import java.util.Map;
 
 public class UserHandler {
-    private final UserDataAccess userDataAccess;
-    private final AuthDataAccess authDataAccess;
-    //private final UserService userService; ????????????????????????????????????
+    private final UserService userService;
 
-    public UserHandler(UserDataAccess userDataAccess, AuthDataAccess authDataAccess) {
-        this.userDataAccess = userDataAccess;
-        this.authDataAccess = authDataAccess;
-        //this.userService = userService; ????????????????????????????????????
+    public UserHandler(UserService userService) {
+        this.userService = userService;
     }
 
-    public String registerHandler(Request req, Response res) {
-        try {
-            // build the service
-            UserService userService = new UserService(this.userDataAccess, this.authDataAccess);
-            // take in the HTTP JSON request, turn it into a RegisterRequest object
-            RegisterRequest registerRequest = new Gson().fromJson(req.body(), RegisterRequest.class);
-            // use the appropriate service's method
-            RegisterResponse registerResponse = userService.registerService(registerRequest);
-            // convert the service response to json for the res body
-            res.status(200);
-            return new Gson().toJson(registerResponse);
-        } catch (AlreadyTakenException e) {
-            res.status(403);
-            return new Gson().toJson(Map.of("message", e.getMessage()));
-        } catch (BadRequestException e) {
-            res.status(400);
-            return new Gson().toJson(Map.of("message", e.getMessage()));
-        } catch (DataAccessException e) {
-            res.status(500);
-            return new Gson().toJson(Map.of("message", e.getMessage()));
-        }
+    public String registerHandler(Request req, Response res) throws BadRequestException, AlreadyTakenException,
+            DataAccessException {
+        // take in the HTTP JSON request, turn it into a RegisterRequest object
+        RegisterRequest registerRequest = new Gson().fromJson(req.body(), RegisterRequest.class);
+        // use the appropriate service's method
+        RegisterResponse registerResponse = this.userService.registerService(registerRequest);
+        // convert the service response to json for the res body
+        res.status(200);
+        return new Gson().toJson(registerResponse);
     }
 
-    public String loginHandler(Request req, Response res) throws DataAccessException {
-        try {
-            UserService userService = new UserService(this.userDataAccess, this.authDataAccess);
+    public String loginHandler(Request req, Response res) throws DataAccessException, UnauthorizedException {
+        LoginRequest loginRequest = new Gson().fromJson(req.body(), LoginRequest.class);
+        LoginResponse loginResponse = this.userService.loginService(loginRequest);
 
-            LoginRequest loginRequest = new Gson().fromJson(req.body(), LoginRequest.class);
-            LoginResponse loginResponse = userService.loginService(loginRequest);
-
-            res.status(200);
-            return new Gson().toJson(loginResponse);
-        } catch (UnauthorizedException e) {
-            res.status(401);
-            return new Gson().toJson(Map.of("message", e.getMessage()));
-        }
+        res.status(200);
+        return new Gson().toJson(loginResponse);
     }
 }
 
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -7,7 +7,10 @@ import dataAccess.interfaces.UserDataAccess;
 import dataAccess.memory.MemoryAuthDataAccess;
 import dataAccess.memory.MemoryUserDataAccess;
 import exceptions.AlreadyTakenException;
+import exceptions.BadRequestException;
+import exceptions.UnauthorizedException;
 import handlers.UserHandler;
+import service.UserService;
 import spark.*;
 
 import java.util.Map;
@@ -22,21 +25,30 @@ public class Server {
         // prepare the necessary data access objects
         UserDataAccess userDataAccess = new MemoryUserDataAccess();
         AuthDataAccess authDataAccess = new MemoryAuthDataAccess();
-        // prepare the services ???????????????????????????????????????????????????????????????????????????????????????
-        // UserService userService = new UserService(userDataAccess, authDataAccess); ????????????????????????????????
+        // prepare the services
+        UserService userService = new UserService(userDataAccess, authDataAccess);
         // prepare the handlers
-        UserHandler userHandler = new UserHandler(userDataAccess, authDataAccess);
+        UserHandler userHandler = new UserHandler(userService);
 
         // Register your endpoints and handle exceptions here.
         Spark.post("/user", userHandler::registerHandler);
         Spark.post("/session", userHandler::loginHandler);
 
+        // exceptions
         Spark.exception(DataAccessException.class, (e, request, response) -> {
             response.status(500);
             response.body(new Gson().toJson(Map.of("message", e.getMessage())));
         });
         Spark.exception(AlreadyTakenException.class, (e, request, response) -> {
             response.status(403);
+            response.body(new Gson().toJson(Map.of("message", e.getMessage())));
+        });
+        Spark.exception(BadRequestException.class, (e, request, response) -> {
+            response.status(400);
+            response.body(new Gson().toJson(Map.of("message", e.getMessage())));
+        });
+        Spark.exception(UnauthorizedException.class, (e, request, response) -> {
+            response.status(401);
             response.body(new Gson().toJson(Map.of("message", e.getMessage())));
         });
 
