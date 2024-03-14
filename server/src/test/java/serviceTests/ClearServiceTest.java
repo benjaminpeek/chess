847de674e0d4227faae64117b8dd8fe61244833c
Benjamin Peek <benjaminpeek8@gmail.com>
2024-03-14 12:43:00 -0600

    wrote and passed all junit tests

--- server/src/test/java/serviceTests/ClearServiceTest.java
+++ server/src/test/java/serviceTests/ClearServiceTest.java
@@ -1,17 +1,48 @@
 package serviceTests;
 
-import org.junit.jupiter.api.BeforeEach;
+import dataAccess.DataAccessException;
+import dataAccess.interfaces.AuthDataAccess;
+import dataAccess.interfaces.GameDataAccess;
+import dataAccess.interfaces.UserDataAccess;
+import dataAccess.memory.MemoryAuthDataAccess;
+import dataAccess.memory.MemoryGameDataAccess;
+import dataAccess.memory.MemoryUserDataAccess;
+import exceptions.AlreadyTakenException;
+import exceptions.BadRequestException;
+import exceptions.UnauthorizedException;
 import org.junit.jupiter.api.Test;
+import request.CreateGameRequest;
+import request.RegisterRequest;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClearServiceTest {
 
-    @BeforeEach
-    void setUp() {
-    }
-
     @Test
-    void clearApplicationService() {
+    void clearApplicationService() throws DataAccessException, UnauthorizedException, BadRequestException, AlreadyTakenException {
+        AuthDataAccess authDataAccess = new MemoryAuthDataAccess();
+        UserDataAccess userDataAccess = new MemoryUserDataAccess();
+        GameDataAccess gameDataAccess = new MemoryGameDataAccess(authDataAccess);
+
+        GameService gameService = new GameService(gameDataAccess, authDataAccess);
+        UserService userService = new UserService(userDataAccess, authDataAccess);
+        ClearService clearService = new ClearService(userDataAccess, authDataAccess, gameDataAccess);
+
+        String registerAuthToken = userService.registerService(new RegisterRequest("ben", "pass",
+                "email")).authToken();
+        gameService.createGameService(new CreateGameRequest("game1"), registerAuthToken);
+
+        assertNotNull(authDataAccess.getAuth(registerAuthToken));
+        assertNotNull(userDataAccess.getUser("ben"));
+        assertNotNull(gameDataAccess.getGame(1));
+
+        clearService.clearApplicationService("");
+
+        assertNull(authDataAccess.getAuth(registerAuthToken));
+        assertNull(userDataAccess.getUser("ben"));
+        assertNull(gameDataAccess.getGame(1));
     }
 }
--- server/src/test/java/serviceTests/UserServiceTest.java
+++ server/src/test/java/serviceTests/UserServiceTest.java
@@ -1,23 +1,94 @@
 package serviceTests;
 
-import org.junit.jupiter.api.BeforeEach;
+import dataAccess.DataAccessException;
+import dataAccess.interfaces.AuthDataAccess;
+import dataAccess.interfaces.UserDataAccess;
+import dataAccess.memory.MemoryAuthDataAccess;
+import dataAccess.memory.MemoryUserDataAccess;
+import exceptions.AlreadyTakenException;
+import exceptions.BadRequestException;
+import exceptions.UnauthorizedException;
+import model.UserData;
 import org.junit.jupiter.api.Test;
+import request.LoginRequest;
+import request.RegisterRequest;
+import response.LoginResponse;
+import service.UserService;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void registerServiceSuccess() throws DataAccessException, BadRequestException, AlreadyTakenException {
+        AuthDataAccess authDataAccess = new MemoryAuthDataAccess();
+        UserDataAccess userDataAccess = new MemoryUserDataAccess();
+        UserService userService = new UserService(userDataAccess, authDataAccess);
+
+        assertNull(userDataAccess.getUser("bensleepr"));
+
+        userService.registerService(new RegisterRequest("bensleepr", "passwizz", "ben8@gmail.com"));
+        UserData expected = new UserData("bensleepr", "passwizz", "ben8@gmail.com");
+
+        assertEquals(expected, userDataAccess.getUser("bensleepr"));
     }
 
     @Test
-    void registerService() {
+    void registerServiceThrowsAlreadyTaken() throws BadRequestException, AlreadyTakenException, DataAccessException {
+        AuthDataAccess authDataAccess = new MemoryAuthDataAccess();
+        UserDataAccess userDataAccess = new MemoryUserDataAccess();
+        UserService userService = new UserService(userDataAccess, authDataAccess);
+
+        userService.registerService(new RegisterRequest("bensleepr", "passwizz", "ben8@gmail.com"));
+        assertThrows(AlreadyTakenException.class, () -> userService.registerService(new RegisterRequest("bensleepr",
+                "passwizz", "ben8@gmail.com")));
     }
 
     @Test
-    void loginService() {
+    void loginServiceSuccess() throws BadRequestException, AlreadyTakenException, DataAccessException, UnauthorizedException {
+        AuthDataAccess authDataAccess = new MemoryAuthDataAccess();
+        UserDataAccess userDataAccess = new MemoryUserDataAccess();
+        UserService userService = new UserService(userDataAccess, authDataAccess);
+
+        userService.registerService(new RegisterRequest("bensleepr", "passwizz", "ben8@gmail.com"));
+        assertNotNull(userDataAccess.getUser("bensleepr"));
+        assertEquals("bensleepr", userService.loginService(new LoginRequest("bensleepr", "passwizz")).username());
     }
 
     @Test
-    void logoutService() {
+    void loginServiceThrowsUnauthorized() throws BadRequestException, AlreadyTakenException, DataAccessException {
+        AuthDataAccess authDataAccess = new MemoryAuthDataAccess();
+        UserDataAccess userDataAccess = new MemoryUserDataAccess();
+        UserService userService = new UserService(userDataAccess, authDataAccess);
+
+        userService.registerService(new RegisterRequest("bensleepr", "passwizz", "ben8@gmail.com"));
+        assertThrows(UnauthorizedException.class, () -> userService.loginService(new LoginRequest("bensleepr",
+                "wrongPass")));
+    }
+
+    @Test
+    void logoutServiceSuccess() throws BadRequestException, AlreadyTakenException, DataAccessException, UnauthorizedException {
+        AuthDataAccess authDataAccess = new MemoryAuthDataAccess();
+        UserDataAccess userDataAccess = new MemoryUserDataAccess();
+        UserService userService = new UserService(userDataAccess, authDataAccess);
+
+        userService.registerService(new RegisterRequest("bensleepr", "passwizz", "ben8@gmail.com"));
+        LoginResponse loginResponse = userService.loginService(new LoginRequest("bensleepr", "passwizz"));
+        assertNotNull(authDataAccess.getAuth(loginResponse.authToken()));
+
+        userService.logoutService(loginResponse.authToken());
+        assertNull(authDataAccess.getAuth(loginResponse.authToken()));
+    }
+
+    @Test
+    void logoutServiceThrows() throws BadRequestException, AlreadyTakenException, DataAccessException, UnauthorizedException {
+        AuthDataAccess authDataAccess = new MemoryAuthDataAccess();
+        UserDataAccess userDataAccess = new MemoryUserDataAccess();
+        UserService userService = new UserService(userDataAccess, authDataAccess);
+
+        userService.registerService(new RegisterRequest("bensleepr", "passwizz", "ben8@gmail.com"));
+        userService.loginService(new LoginRequest("bensleepr", "passwizz"));
+
+        assertThrows(UnauthorizedException.class, () -> userService.logoutService("wrongAuth"));
     }
 }
