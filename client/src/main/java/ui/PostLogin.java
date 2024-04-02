803a17e7c9214c6842d5f9a353571e03cbbf4696
Benjamin Peek <benjaminpeek8@gmail.com>
2024-04-01 22:58:45 -0600

    wrote all ui methods

--- client/src/main/java/ui/PostLogin.java
+++ client/src/main/java/ui/PostLogin.java
@@ -1,16 +1,23 @@
 package ui;
 
+import clientRepl.Repl;
 import exceptions.ResponseException;
+import model.GameData;
 import request.CreateGameRequest;
 import request.JoinGameRequest;
 import serverFacade.ServerFacade;
 
 import java.util.Arrays;
+import java.util.Collection;
+
+import static visual.EscapeSequences.RESET_TEXT_COLOR;
 
 public class PostLogin implements UI {
     private final ServerFacade serverFacade;
+    private final String serverUrl;
     public PostLogin(String serverUrl) {
         this.serverFacade = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
     }
 
     @Override
@@ -20,12 +27,12 @@ public class PostLogin implements UI {
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
-                case "creategame" -> createGame(params);
-                case "joingame" -> joinGame(params);
-//                case "observe" -> observeGame(params);
-//                case "listgames" -> listGames(params);
-//                case "logout" -> logout();
-//                case "clear" -> clear();
+                case "create" -> createGame(params);
+                case "join" -> joinGame(params);
+                case "observe" -> observeGame(params);
+                case "list" -> listGames(params);
+                case "logout" -> logout();
+                case "clear" -> clear();
                 case "quit" -> "quit";
                 default -> help();
             };
@@ -51,26 +58,64 @@ public class PostLogin implements UI {
         if (params.length >= 1) {
             String gameID = params[1];
             String playerColor = params[0];
-            serverFacade.joinGame(new JoinGameRequest(playerColor, Integer.parseInt(gameID)));
+            serverFacade.joinGame(new JoinGameRequest(playerColor.toUpperCase(), Integer.parseInt(gameID)));
             return String.format("Joined game %s as %s", gameID, playerColor);
         }
         throw new ResponseException(400, "Expected: <playerColor: WHITE or BLACK> <game ID>");
     }
 
-//    public String clear(String... params) throws ResponseException {
-//        if (params.length == 0) {
-//            serverFacade.clear();
-//            return "cleared database";
-//        }
-//        throw new ResponseException(400, "Expected: <nothing>");
-//    }
+    public String observeGame(String... params) throws ResponseException {
+        if (params.length >= 1) {
+            String gameID = params[0];
+            serverFacade.joinGame(new JoinGameRequest(null, Integer.parseInt(gameID)));
+            return String.format("Joined game %s as an observer", gameID);
+        }
+        throw new ResponseException(400, "Expected: <game ID>");
+    }
+
+    public String listGames(String... params) throws ResponseException {
+        if (params.length == 0) {
+            StringBuilder gamesString = new StringBuilder();
+            Collection<GameData> allGames = serverFacade.listGames().games();
+            for (GameData gameData : allGames) {
+                gamesString.append(gameData.toString());
+                gamesString.append("\n");
+            }
+            return gamesString.toString();
+        }
+        throw new ResponseException(400, "Failed to list games");
+    }
+
+    public String logout(String... params) throws ResponseException {
+        if (params.length == 0) {
+            try {
+                serverFacade.logout();
+                Repl.currentUI = new PreLogin(serverUrl);
+                System.out.print(RESET_TEXT_COLOR);
+            } catch (ResponseException e) {
+                return e.getMessage();
+            }
+            System.out.print(Repl.currentUI.help());
+            return "Successfully logged out";
+        }
+        throw new ResponseException(400, "Failed to logout user");
+    }
+
+    public String clear(String... params) throws ResponseException {
+        if (params.length == 0) {
+            serverFacade.clear();
+            return "cleared database";
+        }
+        throw new ResponseException(400, "Expected: <nothing>");
+    }
 
     @Override
     public String help() {
         return """
-             - creategame - create a new chess game with <gameName>
-             - joingame - join an existing chess game <gameID>
-             - listgames - list all the current chess games
+             - create - create a new chess game with <gameName>
+             - join - join an existing chess game <playerColor: WHITE or BLACK> <gameID>
+             - observe - join a game as an observer <gameID>
+             - list - list all the current chess games
              - logout - logout of your current session
              - quit - exit the program
              - help - explains what each command does
