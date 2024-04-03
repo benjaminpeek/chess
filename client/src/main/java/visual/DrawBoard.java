dd452b8f0d1360877be354f469ea99b9689a980a
Benjamin Peek <benjaminpeek8@gmail.com>
2024-04-03 12:05:57 -0600

    added helper methods to DrawBoard, switch bg/text color

--- client/src/main/java/visual/DrawBoard.java
+++ client/src/main/java/visual/DrawBoard.java
@@ -2,11 +2,15 @@ package visual;
 
 import chess.ChessBoard;
 import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 import static visual.EscapeSequences.*;
 
 public class DrawBoard {
     ChessGame game;
+    String bgColor;
+    String textColor;
 
     public DrawBoard(ChessGame game) {
         this.game = game;
@@ -15,14 +19,47 @@ public class DrawBoard {
     public void draw() {
         ChessBoard board = this.game.getBoard();
 
-        System.out.println(SET_BG_COLOR_BLACK);
-        System.out.println(SET_TEXT_COLOR + "222m");
+        System.out.print(SET_BG_COLOR_BLACK);
+        System.out.print(SET_TEXT_COLOR + "222m");
         // white on bottom
+        // top row
+        System.out.print(EMPTY);
+        System.out.print(" a ");
+        System.out.print(" b ");
+        System.out.print(" c ");
+        System.out.print(" d ");
+        System.out.print(" e ");
+        System.out.print(" f ");
+        System.out.print(" g ");
+        System.out.println(" h ");
+
+        System.out.print(" 8 ");
+        bgColor = SET_BG_COLOR + "222m";
+        System.out.print(bgColor);
+        textColor = SET_TEXT_COLOR_BLACK;
+        System.out.print(textColor);
         for (int i = 1; i < 9; i++) {
-            System.out.println("yeah this lit");
+            ChessPiece piece = board.getPiece(new ChessPosition(8, i));
+            setTextColor(piece.getTeamColor());
+            System.out.println(" " + "" + " ");
         }
 
-        // black on bottom
+        System.out.println();
+    }
 
+    private void switchBgColor() {
+        if (bgColor.equals(SET_BG_COLOR_BLACK)) {
+            bgColor = SET_BG_COLOR_WHITE;
+        } else if (bgColor.equals(SET_BG_COLOR_WHITE)) {
+            bgColor = SET_BG_COLOR_BLACK;
+        }
+    }
+
+    private void setTextColor(ChessGame.TeamColor color) {
+        if (color == ChessGame.TeamColor.WHITE) {
+            textColor = SET_TEXT_COLOR_WHITE;
+        } else if (color == ChessGame.TeamColor.BLACK) {
+            textColor = SET_TEXT_COLOR_BLACK;
+        }
     }
 }
