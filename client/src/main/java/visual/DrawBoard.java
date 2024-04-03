4a9222097dfc596b37f5f1da746e5d558f37e82f
Benjamin Peek <benjaminpeek8@gmail.com>
2024-04-03 14:08:46 -0600

    board is drawing correctly, white and black pov

--- client/src/main/java/visual/DrawBoard.java
+++ client/src/main/java/visual/DrawBoard.java
@@ -19,49 +19,122 @@ public class DrawBoard {
 
     public void drawWhite() {
         povColor = ChessGame.TeamColor.WHITE;
-        ChessBoard board = this.game.getBoard();
 
-        System.out.print(SET_BG_COLOR_BLACK);
-        System.out.print(SET_TEXT_COLOR + "222m");
         // header letters
-        System.out.print(EMPTY);
-        System.out.print(" a ");
-        System.out.print(" b ");
-        System.out.print(" c ");
-        System.out.print(" d ");
-        System.out.print(" e ");
-        System.out.print(" f ");
-        System.out.print(" g ");
-        System.out.print(" h ");
-        System.out.print(EMPTY);
+        drawLetters();
+
         System.out.print(RESET_BG_COLOR);
         System.out.println();
-
-        // s
-        System.out.print(SET_BG_COLOR_BLACK);
-        System.out.print(" 8 ");
-        bgColor = SET_BG_COLOR + "222m";
-        System.out.print(bgColor);
-        for (int i = 1; i < 9; i++) {
-            ChessPiece piece = board.getPiece(new ChessPosition(8, i));
-            setTextColor(piece.getTeamColor());
-            System.out.print(" " + piece + " ");
-            switchBgColor();
+        // draw the rows
+        for (int i = 8; i > 0; i--) {
+            drawRow(this.game.getBoard(), i);
         }
-        System.out.print(SET_BG_COLOR_BLACK);
-        System.out.print(SET_TEXT_COLOR + "222m");
-        System.out.print(" 8 ");
+        // footer letters
+        drawLetters();
+
         System.out.print(RESET_BG_COLOR);
+        System.out.println();
 
         System.out.println();
     }
 
     public void drawBlack() {
         povColor = ChessGame.TeamColor.BLACK;
+
+        // header letters
+        drawLetters();
+
+        System.out.print(RESET_BG_COLOR);
+        System.out.println();
+        // draw the rows
+        for (int i = 1; i < 9; i++) {
+            drawRow(this.game.getBoard(), i);
+        }
+        // footer letters
+        drawLetters();
+
+        System.out.print(RESET_BG_COLOR);
+        System.out.println();
+
+        System.out.println();
     }
 
-    private void drawHeaders() {
+    private void drawLetters() {
+        System.out.print(SET_BG_COLOR_BLACK);
+        System.out.print(SET_TEXT_COLOR + "222m");
+        System.out.print(EMPTY);
+        switch (povColor) {
+            case WHITE -> {
+                System.out.print(" a ");
+                System.out.print(" b ");
+                System.out.print(" c ");
+                System.out.print(" d ");
+                System.out.print(" e ");
+                System.out.print(" f ");
+                System.out.print(" g ");
+                System.out.print(" h ");
+            }
+            case BLACK -> {
+                System.out.print(" h ");
+                System.out.print(" g ");
+                System.out.print(" f ");
+                System.out.print(" e ");
+                System.out.print(" d ");
+                System.out.print(" c ");
+                System.out.print(" b ");
+                System.out.print(" a ");
+            }
+        }
+        System.out.print(EMPTY);
+    }
 
+    private void drawRow(ChessBoard board, int row) {
+        System.out.print(SET_BG_COLOR_BLACK);
+        System.out.print(" " + row + " ");
+
+        if (row % 2 == 0) {bgColor = SET_BG_COLOR + "222m";}
+        else {bgColor = SET_BG_COLOR + "95m";}
+        System.out.print(bgColor);
+
+        switch (povColor) {
+            case WHITE -> {
+                for (int i = 1; i < 9; i++) {
+                    drawSquare(board, row, i);
+                }
+            }
+            case BLACK -> {
+                for (int i = 8; i > 0; i--) {
+                    drawSquare(board, row, i);
+                }
+            }
+        }
+//        for (int i = 1; i < 9; i++) {
+//            ChessPiece piece = board.getPiece(new ChessPosition(row, i));
+//            if (piece != null) {
+//                setTextColor(piece.getTeamColor());
+//                System.out.print(" " + piece.toString().toUpperCase() + " ");
+//            } else {
+//                System.out.print(EMPTY);
+//            }
+//            switchBgColor();
+//        }
+        System.out.print(SET_BG_COLOR_BLACK);
+        System.out.print(SET_TEXT_COLOR + "222m");
+        System.out.print(" " + row + " ");
+        System.out.print(RESET_BG_COLOR);
+
+        System.out.println();
+    }
+
+    private void drawSquare(ChessBoard board, int row, int col) {
+        ChessPiece piece = board.getPiece(new ChessPosition(row, col));
+        if (piece != null) {
+            setTextColor(piece.getTeamColor());
+            System.out.print(" " + piece.toString().toUpperCase() + " ");
+        } else {
+            System.out.print(EMPTY);
+        }
+        switchBgColor();
     }
 
     private void switchBgColor() {
