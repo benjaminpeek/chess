440b5f7ce4fa5d604007f8f54758dea57f5593b0
Benjamin Peek <benjaminpeek8@gmail.com>
2024-04-09 15:57:04 -0600

    working on Gameplay UI

--- client/src/main/java/ui/Gameplay.java
+++ client/src/main/java/ui/Gameplay.java
@@ -6,6 +6,8 @@ import serverFacade.ServerFacade;
 import webSocket.NotificationHandler;
 import webSocket.WebSocketFacade;
 
+import java.util.Arrays;
+
 public class Gameplay implements UI {
     private final String serverUrl;
     private final WebSocketFacade webSocketFacade;
@@ -15,8 +17,25 @@ public class Gameplay implements UI {
         this.webSocketFacade = new WebSocketFacade(serverUrl, Repl.notificationHandler);
     }
 
-    @Override
+
     public String eval(String input) {
+//        try {
+//            var tokens = input.toLowerCase().split(" ");
+//            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+//            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+//            return switch (cmd) {
+//                case "redraw" -> redrawBoard(params);
+//                case "leave" -> leaveGame(params);
+//                case "makeMove" -> observeGame(params);
+//                case "resign" -> listGames(params);
+//                case "highlight" -> logout();
+//                case "quit" -> "quit";
+//                default -> help();
+//            };
+//        } catch (ResponseException ex) {
+//            return ex.getMessage();
+//        }
+
         return null;
     }
 
