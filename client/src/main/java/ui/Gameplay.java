1f1c6b781d9f2e81b0856a656d7893e40f17a6c4
Benjamin Peek <benjaminpeek8@gmail.com>
2024-04-09 12:59:23 -0600

    added websocket facade to client

---
+++ client/src/main/java/ui/Gameplay.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class Gameplay implements UI {
+    @Override
+    public String eval(String input) {
+        return null;
+    }
+
+    @Override
+    public String help() {
+        return null;
+    }
+}
---
+++ client/src/main/java/webSocket/WebSocketFacade.java
@@ -0,0 +1,13 @@
+package webSocket;
+import javax.websocket.*;
+
+public class WebSocketFacade extends Endpoint {
+    Session session;
+
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+
+    }
+
+}
