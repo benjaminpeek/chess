388926a6638fad3d4b3f257b3bb71786f7dc02c7
Benjamin Peek <benjaminpeek8@gmail.com>
2024-04-09 14:56:46 -0600

    created ServerMessage subclasses

--- shared/src/main/java/webSocketMessages/serverMessages/LoadGame.java
+++ shared/src/main/java/webSocketMessages/serverMessages/LoadGame.java
@@ -1,8 +1,13 @@
 package webSocketMessages.serverMessages;
 
-public class LoadGame extends ServerMessage {
-    public LoadGame(ServerMessageType type) {
-        super(type);
-    }
+import chess.ChessGame;
+import model.GameData;
 
+public class LoadGame extends ServerMessage {
+    GameData game;
+
+    public LoadGame(ServerMessageType type, GameData game) {
+        super(type);
+        this.game = game;
+    }
 }
