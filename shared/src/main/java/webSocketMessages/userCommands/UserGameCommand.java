651ba30224b6777428fe80297f4f487c89593116
Benjamin Peek <benjaminpeek8@gmail.com>
2024-04-10 12:53:32 -0600

    changed equals method of UserGameCommand

--- shared/src/main/java/webSocketMessages/userCommands/UserGameCommand.java
+++ shared/src/main/java/webSocketMessages/userCommands/UserGameCommand.java
@@ -4,7 +4,6 @@ import java.util.Objects;
 
 /**
  * Represents a command a user can send the server over a websocket
- * 
  * Note: You can add to this class, but you should not alter the existing
  * methods.
  */
@@ -39,9 +38,8 @@ public class UserGameCommand {
     public boolean equals(Object o) {
         if (this == o)
             return true;
-        if (!(o instanceof UserGameCommand))
+        if (!(o instanceof UserGameCommand that))
             return false;
-        UserGameCommand that = (UserGameCommand) o;
         return getCommandType() == that.getCommandType() && Objects.equals(getAuthString(), that.getAuthString());
     }
 
