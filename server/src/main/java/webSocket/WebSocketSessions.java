1e8daccc10b3eb115bb6fec432941e3d97989049
Benjamin Peek <benjaminpeek8@gmail.com>
2024-04-10 12:43:15 -0600

    wrote WebSocketSessions class

--- server/src/main/java/webSocket/WebSocketSessions.java
+++ server/src/main/java/webSocket/WebSocketSessions.java
@@ -1,4 +1,32 @@
 package webSocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class WebSocketSessions {
+    Map<Integer, Map<String, Session>> sessionsMap;
+
+    WebSocketSessions() {
+        this.sessionsMap = new HashMap<>();
+    }
+
+
+    public void addSessionToGame(int gameID, String authToken, Session session) {
+        sessionsMap.get(gameID).put(authToken, session);
+    }
+
+    public void removeSessionFromGame(int gameID, String authToken, Session session) {
+        Map<String, Session> game = sessionsMap.get(gameID);
+        game.remove(authToken, session);
+    }
+
+    public void removeSession(Session session) {
+
+    }
+
+    public Map<String, Session> getSessionsForGame(int gameID) {
+        return sessionsMap.get(gameID);
+    }
 }
