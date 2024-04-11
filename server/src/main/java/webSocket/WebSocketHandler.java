68faad9f266d444b5c6ffcb9c2da578cd97dee33
Benjamin Peek <benjaminpeek8@gmail.com>
2024-04-11 09:54:45 -0600

    send error message back to just the root client (i think)

--- server/src/main/java/webSocket/WebSocketHandler.java
+++ server/src/main/java/webSocket/WebSocketHandler.java
@@ -10,6 +10,7 @@ import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import webSocketMessages.serverMessages.*;
+import webSocketMessages.serverMessages.Error;
 import webSocketMessages.userCommands.*;
 
 import java.io.IOException;
@@ -47,10 +48,10 @@ public class WebSocketHandler {
         int gameID = joinPlayerCommand.gameID();
         String authToken = joinPlayerCommand.getAuthString();
         if (authDataAccess.getAuth(authToken) == null) {
-            // auth is not valid, so where do i send the error? do i need to account for this here?
+            session.getRemote().sendString(new Error("invalid auth token").getErrorMessage());
         }
         if (gameDataAccess.getGame(gameID) == null) {
-
+            session.getRemote().sendString(new Error("invalid game ID").getErrorMessage());
         }
 
         GameData game = gameDataAccess.getGame(gameID);
@@ -58,6 +59,7 @@ public class WebSocketHandler {
         webSocketSessions.sendMessage(gameID, new LoadGame(game), authToken);
         webSocketSessions.broadcastMessage(gameID, new Notification(authDataAccess.getAuth(authToken).username()
                 + " joined as " + joinPlayerCommand.playerColor()), authToken);
+        // do i use the gameservice to add the player to the game or just use session methods?
     }
 
     public void joinObserver(Session session, String message) {
@@ -67,14 +69,17 @@ public class WebSocketHandler {
 
     public void makeMove(String message) {
         MakeMove makeMoveCommand = new Gson().fromJson(message, MakeMove.class);
+        // ChessGame/Move logic?
     }
 
     public void leaveGame(String message) {
         LeaveGame leaveGameCommand = new Gson().fromJson(message, LeaveGame.class);
+        // ChessGame/Move logic?
     }
 
     public void resignGame(String message) {
         ResignGame resignGameCommand = new Gson().fromJson(message, ResignGame.class);
+        // ChessGame/Move logic?
     }
 
 
@@ -85,4 +90,7 @@ public class WebSocketHandler {
     // how to broadcast to all users including root client -- does my solution look/work?
     // how to handle observers? where to store them / do I need to?
     // do i use the other service classes in this one to actually add players to the game as necessary? or just the daos
+    // does all the game login happen in this class? making moves, checking for check/mate, winning/losing?
+    // how to handle make move commands, where does the logic go that allows for chess notation to be used to make moves?
+    //  probably in ChessMove, as that is what gets passed in to the MAKE_MOVE user command?
 }
